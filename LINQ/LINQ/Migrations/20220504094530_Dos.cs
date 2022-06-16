2bbfdc222fc60b94fe05c93d4eaf924a967d7567
JeremyCedendahl <100521709+JeremyCedendahl@users.noreply.github.com>
2022-06-16 11:48:25 +0200

    Add files via upload

---
+++ LINQ/LINQ/Migrations/20220504094530_Dos.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LINQ.Migrations
+{
+    public partial class Dos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Studenter");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Studenter");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Teachers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Studenter",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Studenter");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Studenter",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Studenter",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ LINQ/LINQ/Migrations/20220505092652_Tres.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LINQ.Migrations
+{
+    public partial class Tres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentKurs_Kurser_KursId",
+                table: "StudentKurs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentKurs_Studenter_StudentID",
+                table: "StudentKurs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentKurs",
+                table: "StudentKurs");
+
+            migrationBuilder.RenameTable(
+                name: "StudentKurs",
+                newName: "StudentKurser");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentKurs_StudentID",
+                table: "StudentKurser",
+                newName: "IX_StudentKurser_StudentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentKurs_KursId",
+                table: "StudentKurser",
+                newName: "IX_StudentKurser_KursId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentKurser",
+                table: "StudentKurser",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentKurser_Kurser_KursId",
+                table: "StudentKurser",
+                column: "KursId",
+                principalTable: "Kurser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentKurser_Studenter_StudentID",
+                table: "StudentKurser",
+                column: "StudentID",
+                principalTable: "Studenter",
+                principalColumn: "StudentID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentKurser_Kurser_KursId",
+                table: "StudentKurser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentKurser_Studenter_StudentID",
+                table: "StudentKurser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentKurser",
+                table: "StudentKurser");
+
+            migrationBuilder.RenameTable(
+                name: "StudentKurser",
+                newName: "StudentKurs");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentKurser_StudentID",
+                table: "StudentKurs",
+                newName: "IX_StudentKurs_StudentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentKurser_KursId",
+                table: "StudentKurs",
+                newName: "IX_StudentKurs_KursId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentKurs",
+                table: "StudentKurs",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentKurs_Kurser_KursId",
+                table: "StudentKurs",
+                column: "KursId",
+                principalTable: "Kurser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentKurs_Studenter_StudentID",
+                table: "StudentKurs",
+                column: "StudentID",
+                principalTable: "Studenter",
+                principalColumn: "StudentID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
